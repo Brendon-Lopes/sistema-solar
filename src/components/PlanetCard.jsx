4657fb92fbf2038944c2fc83b965fca3ba905bca
Brendon Lopes <brendon.lopes.21@gmail.com>
2022-02-18 17:47:05 -0300

    Requirements 5, 6 and 7 completed

--- src/components/PlanetCard.jsx
+++ src/components/PlanetCard.jsx
@@ -6,9 +6,9 @@ class PlanetCard extends React.Component {
     const { planetName, planetImage } = this.props;
 
     return (
-      <div data-testid="planet-card">
-        <p data-testid="planet-name">{ planetName }</p>
+      <div className="planet-card" data-testid="planet-card">
         <img src={ planetImage } alt={ `Planeta ${planetName}` } />
+        <p data-testid="planet-name">{ planetName }</p>
       </div>
     );
   }
--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import Title from './Title';
+import PlanetCard from './PlanetCard';
+import Planets from '../data/planets';
 
 class SolarSystem extends React.Component {
   render() {
+    const planets = Planets.map(({ name, image }) => (<PlanetCard
+      key={ name }
+      planetName={ name }
+      planetImage={ image }
+    />));
+
     return (
-      <div className="title-container" data-testid="solar-system">
+      <div className="solar-system-container" data-testid="solar-system">
         <Title headline="Planetas" />
+        <div className="planet-cards-container">
+          {planets}
+        </div>
       </div>
     );
   }
